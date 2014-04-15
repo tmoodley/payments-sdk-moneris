eec59a32ea7a5e17f3832e4c940e2d36205eb2b3
rootzid <rootzid@gmail.com>
2014-04-15 16:49:06 +0400

    Add untracked files

---
+++ PaymentsSdk.USMoneris/Request/USPurchaseBasic.cs
@@ -0,0 +1,33 @@
+﻿namespace Rootzid.PaymentsSdk.USMoneris.Request
+{
+    using System;
+    using global::USMoneris;
+    using Moneris;
+    using Response;
+
+    public class USPurchaseBasic
+    {
+        private readonly IMonerisCredentials credentials;
+
+        public USPurchaseBasic(IMonerisCredentials credentials)
+        {
+            this.credentials = credentials;
+        }
+
+        public IMonerisResponse Send(string orderId, string amount)
+        {
+            var purchase = new USPurchase(orderId, amount, "4242424242424242", "0812", "7", "INVC090", "1.00");
+            var request = new HttpsPostRequest(credentials.Host, credentials.StoreId, credentials.ApiToken, purchase);
+
+            try
+            {
+                var receipt = request.GetReceipt();
+                return new USResponse(receipt);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ PaymentsSdk.USMoneris/Response/USResponse.cs
@@ -0,0 +1,54 @@
+﻿namespace Rootzid.PaymentsSdk.USMoneris.Response
+{
+    using global::USMoneris;
+    using Moneris;
+
+    internal class USResponse : IMonerisResponse
+    {
+        public string ReceiptId { get; private set; }
+        public string ReferenceNum { get; private set; }
+        public string ResponseCode { get; private set; }
+        public string Iso { get; private set; }
+        public string AuthCode { get; private set; }
+        public string TransTime { get; private set; }
+        public string TransDate { get; private set; }
+        public string TransType { get; private set; }
+        public string Complete { get; private set; }
+        public string Message { get; private set; }
+        public string TransAmount { get; private set; }
+        public string CardType { get; private set; }
+        public string TxnNumber { get; private set; }
+        public string TimedOut { get; private set; }
+        public string RecurSucess { get; private set; }
+        public string AvsResultCode { get; private set; }
+        public string CvdResultCode { get; private set; }
+        public string CavvResultCode { get; private set; }
+
+        public USResponse(Receipt receipt)
+        {
+            this.Initialize(receipt);
+        }
+
+        private void Initialize(Receipt r)
+        {
+            this.ReceiptId = r.GetReceiptId();
+            this.ReferenceNum = r.GetReferenceNum();
+            this.ResponseCode = r.GetResponseCode();
+            this.Iso = r.GetISO();
+            this.AuthCode = r.GetAuthCode();
+            this.TransTime = r.GetTransTime();
+            this.TransDate = r.GetTransDate();
+            this.TransType = r.GetTransType();
+            this.Complete = r.GetComplete();
+            this.Message = r.GetMessage();
+            this.TransAmount = r.GetTransAmount();
+            this.CardType = r.GetCardType();
+            this.TxnNumber = r.GetTxnNumber();
+            this.TimedOut = r.GetTimedOut();
+            this.RecurSucess = r.GetRecurSuccess();
+            this.AvsResultCode = r.GetAvsResultCode();
+            this.CvdResultCode = r.GetCvdResultCode();
+            this.CavvResultCode = r.GetCavvResultCode();
+        }
+    }
+}
