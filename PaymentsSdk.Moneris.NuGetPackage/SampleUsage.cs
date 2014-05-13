cfbb3545c3f5d5bb207034f90db3898e79a0238f
rootzid <rootzid@gmail.com>
2014-05-13 16:42:33 +0400

    Update readme file

--- PaymentsSdk.Moneris.NuGetPackage/SampleUsage.cs
+++ PaymentsSdk.Moneris.NuGetPackage/SampleUsage.cs
@@ -5,7 +5,7 @@
 
     class SampleUsage
     {
-        public void TestMoneris()
+        public void TestCanadianMoneris()
         {
             var credentials = new Credentials
             {
@@ -34,5 +34,37 @@
                 receipt.Message, 
                 receipt.TxnNumber);
         }
+
+/*
+        public void TestUsaMoneris()
+        {
+            var credentials = new Credentials
+            {
+                Host = "esqa.moneris.com",
+                ApiToken = "yesguy",
+                StoreId = "store5"
+            };
+
+            var creditCard = new CreditCard()
+            {
+                Pan = "4242424242424242",
+                ExpDate = new DateTime(2018, 12, 10)
+            };
+
+            var order = new Order()
+            {
+                OrderId = "Test_Order_123456",
+                Amount = 5.00m
+            };
+
+            var gateway = new USGateway(credentials);
+            var receipt = gateway.Purchase(creditCard, order);
+
+            Console.WriteLine("Transaction result: ResponseCode={0}, Message={1}, TxnNumber={2}", 
+                receipt.ResponseCode, 
+                receipt.Message, 
+                receipt.TxnNumber);
+        }
+*/
     }
 }
